814fa13468114ea8d993ecf4e067c66a604208df
ROhta <13445921+ROhta@users.noreply.github.com>
2022-06-26 22:33:09 +0900

    warning対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 import IndexPage from "../templates"
 
 const Home: NextPage = () => {
@@ -22,13 +22,10 @@ const Home: NextPage = () => {
         <meta property="og:type" content="game"/>
         <title>Bingo Machine</title>
         <link rel="icon" href="./logo.ico"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
-        <link href="https://fonts.googleapis.com/css?family=MedievalSharp&amp;subset=latin-ext" rel="stylesheet"/>
       </Head>
 
       <main>
         <IndexPage />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
       </main>
     </div>
   )
