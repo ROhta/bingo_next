72839d892ec5ae03a04d4467e4b28a02cdc84cb8
RO <13445921+ROhta@users.noreply.github.com>
2022-06-26 10:41:50 +0900

    src配下にソースを移動 (#58)
    * src配下に移す
    
    * mdじゃなかった

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import IndexPage from "../templates/Index"
+import styles from '../../styles/Home.module.css'
+import IndexPage from "../templates"
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
       <Head>
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0"/>
-        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
         <meta name="description" content="Bingo Machine"/>
         <meta name="author" content="ROhta"/>
         <meta name="robots" content="noindex, nofollow"/>
