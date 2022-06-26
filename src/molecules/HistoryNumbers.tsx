caa9be6ad8f93357faca148f082e18ff2184d8b3
ROhta <13445921+ROhta@users.noreply.github.com>
2022-06-26 22:55:36 +0900

    コンソールエラー対応

--- src/molecules/HistoryNumbers.tsx
+++ src/molecules/HistoryNumbers.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import type { FC } from "react"
 import HistoryNumber from "../atoms/HistoryNumber"
 
 const HistoryNumbers: FC = () => {
--- src/molecules/OperationButtons.tsx
+++ src/molecules/OperationButtons.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import type { FC } from "react"
 import StartButton from "../atoms/StartButton"
 import ResetButton from "../atoms/ResetButton"
 
