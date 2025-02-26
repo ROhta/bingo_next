b2e533aca6f2384a7ea65faf4bc18cb8594e5ac8
RO <13445921+ROhta@users.noreply.github.com>
2025-02-26 17:07:15 +0900

    feat: Storybookの設定を更新し、MDX GFMアドオンを追加
    - package.jsonに@storybook/addon-mdx-gfmを追加
    - .storybook/main.tsにMDX GFMアドオンを含む設定を追加
    - ドキュメント自動生成を有効化
    - TypeScriptの設定を追加

--- application/.storybook/main.ts
+++ application/.storybook/main.ts
@@ -2,11 +2,22 @@ import type {StorybookConfig} from "@storybook/nextjs"
 
 const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-	addons: ["@storybook/addon-onboarding", "@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions"],
+
+	addons: ["@storybook/addon-onboarding", "@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+
 	framework: {
 		name: "@storybook/nextjs",
 		options: {},
 	},
+
 	staticDirs: ["../public"],
+
+	docs: {
+		autodocs: true,
+	},
+
+	typescript: {
+		reactDocgen: "react-docgen-typescript",
+	},
 }
 export default config
