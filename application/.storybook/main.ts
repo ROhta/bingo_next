6bdd41909d7c4597bdb220205ba16ec44f3ccd9f
RO <13445921+ROhta@users.noreply.github.com>
2025-02-28 22:00:05 +0900

    feat: Storybookの設定を更新し、Vitestとの統合を追加
    - vitest.setup.tsを新規作成し、Storybookのテスト設定を追加
    - main.tsで@storybook/experimental-nextjs-viteを使用するように変更
    - vitest.config.tsを新規作成し、Storybookのテストプラグインを設定
    - package.jsonにVitestのテストスクリプトを追加し、依存関係を更新

--- application/.storybook/main.ts
+++ application/.storybook/main.ts
@@ -1,12 +1,12 @@
-import type {StorybookConfig} from "@storybook/nextjs"
+import type {StorybookConfig} from "@storybook/experimental-nextjs-vite"
 
 const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
-	addons: ["@storybook/addon-onboarding", "@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+	addons: ["@storybook/addon-onboarding", "@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-mdx-gfm", "@storybook/experimental-addon-test"],
 
 	framework: {
-		name: "@storybook/nextjs",
+		name: "@storybook/experimental-nextjs-vite",
 		options: {},
 	},
 
