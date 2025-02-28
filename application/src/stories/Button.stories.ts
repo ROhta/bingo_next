49eb087b524b1a3d34cf55ade7d65c6ac0c21d9b
RO <13445921+ROhta@users.noreply.github.com>
2025-02-28 13:37:41 +0900

    feat: Chromaticのスクリプトを更新し、変更されたファイルのみを対象にするように設定
    - package.json: chromaticスクリプトを変更し、--only-changedオプションを追加
    - Button.stories.ts: chromaticのスナップショットを無効にする設定を追加

--- application/src/stories/Button.stories.ts
+++ application/src/stories/Button.stories.ts
@@ -7,6 +7,9 @@ const meta: Meta<typeof Button> = {
 	component: Button,
 	parameters: {
 		layout: "centered",
+		chromatic: {
+			disableSnapshot: true,
+		},
 	},
 	tags: ["autodocs"],
 	args: {onClick: fn()},
