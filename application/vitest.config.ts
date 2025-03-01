0bfc7b9f6603d00fcab8e3fc5540141f0a315c24
RO <13445921+ROhta@users.noreply.github.com>
2025-03-01 21:52:40 +0900

    chore: #527 Chromaticワークフローとテスト設定を更新
    - Chromaticアクションに`onlyChanged`と`skip`オプションを追加
    - Chromaticワークフローの作業ディレクトリを`application`に設定
    - vitest.config.tsにStorybookテストプラグインを再導入
    - postcss.config.mjsの末尾の改行を微調整

--- application/vitest.config.ts
+++ application/vitest.config.ts
@@ -1,7 +1,7 @@
 import path from "node:path"
 import {fileURLToPath} from "node:url"
-
 import {defineConfig} from "vitest/config"
+import {storybookTest} from "@storybook/experimental-addon-test/vitest-plugin"
 
 const dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
@@ -13,5 +13,30 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(dirname, "./src"),
 		},
+		workspace: [
+			{
+				extends: true,
+				plugins: [
+					// The plugin will run tests for the stories defined in your Storybook config
+					// See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+					storybookTest({
+						configDir: path.join(dirname, ".storybook"),
+						tags: {
+							exclude: ["skip"],
+						},
+					}),
+				],
+				test: {
+					name: "storybook",
+					browser: {
+						enabled: true,
+						headless: true,
+						name: "chromium",
+						provider: "playwright",
+					},
+					setupFiles: [".storybook/vitest.setup.ts"],
+				},
+			},
+		],
 	},
 })
