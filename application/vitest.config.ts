ad3f8643e4355ec6a8730a5b54512e037c8141c4
RO <13445921+ROhta@users.noreply.github.com>
2025-03-01 14:49:06 +0900

    refactor: #527 Vitestの設定からStorybookテストプラグインを削除する
    - vitest.config.tsからStorybookのテストプラグインのインポートをコメントアウト
    - 不要になったStorybookテストプラグインのコードを削除

--- application/vitest.config.ts
+++ application/vitest.config.ts
@@ -3,8 +3,6 @@ import {fileURLToPath} from "node:url"
 
 import {defineConfig} from "vitest/config"
 
-// import {storybookTest} from "@storybook/experimental-addon-test/vitest-plugin"
-
 const dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
