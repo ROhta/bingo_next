d3b4c755dd385af44f243144f1ba96ca752dcaff
RO <13445921+ROhta@users.noreply.github.com>
2025-03-03 12:30:19 +0900

    fix: HCP変数を追加

--- iac/variables.tf
+++ iac/variables.tf
@@ -2,3 +2,15 @@ variable "vercel_api_token" {
   description = "API token for Vercel"
   type        = string
 }
+
+variable "HCP_CLIENT_ID" {
+  description = "HCP Client ID"
+  type        = string
+  default     = ""
+}
+
+variable "HCP_CLIENT_SECRET" {
+  description = "HCP Client Secret"
+  type        = string
+  default     = ""
+}
