8292cb3ef572e8563f0c4e8fc8856bcb30cfe527
RO <13445921+ROhta@users.noreply.github.com>
2025-03-03 13:16:26 +0900

    Fix (#530)
    * chore: #533 HCP Vault Secretsの設定を削除する
    
    - main.tfからHCP Vault Secretsのデータソース設定をコメントアウト
    - output.tfからHCP Vault Secretsの出力をコメントアウト
    - provider.tfからHCPプロバイダーの設定をコメントアウト
    - variables.tfからHCP関連の変数をコメントアウト
    - Vercelプロバイダーの認証方法をVercelのAPIトークン変数に変更
    
    * chore: #534 Terraformの設定からHCP Vault関連の設定を完全に削除
    
    - main.tfからHCP Vault Secretsのデータソースのコメントを完全に削除
    - output.tfを完全に削除
    - provider.tfからHCPプロバイダーのコメントを完全に削除
    - variables.tfからHCP関連の変数のコメントを完全に削除
    - Vercelプロバイダーの認証方法をAPIトークン変数に固定
    
    * fix: HCP変数を追加
    
    * fix: #535 HCP変数の説明を更新する
    
    - HCP_CLIENT_IDとHCP_CLIENT_SECRETの説明を「Service Principals/Keys」に更新
    - デフォルト値を削除し、変数の必須化を明確にする
    
    * feat: #536 HCPプロバイダーを追加する
    
    - provider.tfにHCPプロバイダーを追加
    - HCP_CLIENT_IDとHCP_CLIENT_SECRETを使用してHCPプロバイダーを設定
    - Vercelプロバイダーの設定は変更なし
    
    * chore: #537 HCPプロバイダーを削除する
    
    - main.tfからHCPプロバイダーを削除
    - provider.tfからHCPプロバイダーの設定を削除
    - variables.tfからHCP関連の変数を削除

--- iac/main.tf
+++ iac/main.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "vercel/vercel"
       version = "~> 2.10"
     }
-    hcp = {
-      source  = "hashicorp/hcp"
-      version = "0.104.0"
-    }
   }
 
   backend "remote" {
@@ -20,8 +16,4 @@ terraform {
       name = "bingo_next_iac"
     }
   }
-}
-
-data "hcp_vault_secrets_app" "bingo_next" {
-  app_name = "bingo-next"
 }
--- iac/output.tf
+++
@@ -1,4 +0,0 @@
-output "secrets" {
-  value     = data.hcp_vault_secrets_app.bingo_next.secrets
-  sensitive = true
-}
