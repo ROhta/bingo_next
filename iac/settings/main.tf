d00a934c341039469729f454acae42e5cffa0615
RO <13445921+ROhta@users.noreply.github.com>
2025-03-04 17:42:16 +0900

    feat: #541 HCPのVault Secretsアプリケーション名を更新する
    - HCP Vault Secretsのアプリケーション名を`bingo_next_deploy`から`chromatic`に変更
    - リソース名を`hcp_vault_secrets_app.bingo_next_deploy`から`hcp_vault_secrets_app.chromatic`に更新

--- iac/settings/main.tf
+++ iac/settings/main.tf
@@ -37,7 +37,7 @@ variable "client_secret" {
 
 
 
-resource "hcp_vault_secrets_app" "bingo_next_deploy" {
+resource "hcp_vault_secrets_app" "chromatic" {
   app_name   = "chromatic"
   project_id = "54ecb58d-35f2-4a71-9152-79b82af95578"
 }
