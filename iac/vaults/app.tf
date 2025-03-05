dd0333ad0d383f879798a8ff95176b7586af1f00
RO <13445921+ROhta@users.noreply.github.com>
2025-03-05 21:17:34 +0900

    refactor: #543 HCP Vault Secretsのアプリケーション設定を最適化する
    - locals.tfの内容をapp.tfに統合
    - vault_listとproject_idをapp.tfのlocals内に移動
    - 重複するローカル変数の削除と設定の一元化

--- iac/vaults/app.tf
+++ iac/vaults/app.tf
@@ -4,3 +4,15 @@ resource "hcp_vault_secrets_app" "bingo_next_project" {
   app_name   = each.value
   project_id = local.project_id
 }
+
+locals {
+  project_id = "54ecb58d-35f2-4a71-9152-79b82af95578"
+  vault_list = [
+    "chromatic",
+    "GitHub-Projects",
+    "GitHub-Scan",
+    "hcp",
+    "profile",
+    "vercel-token"
+  ]
+}
